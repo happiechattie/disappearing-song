210df8787e30587e8be7333d78d34b88534d67b1
chattie <christuaik@gmail.com>
2023-05-03 21:01:15 -0700

    functions moved to global scope

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
+function renderPoems(){
+    document.querySelector(".poems").innerText = '';
+    fetch("http://localhost:3000/poems/")
+    .then(r => r.json())
+    .then(data => {
+        console.log(data);
+        data.slice().reverse().forEach(poem => {
+            renderPoem(poem)
+        });
+        console.log('POEMS RENDERED');
+    });
+}
+
+function postPoem (poem) {
+    //add poem to db.json
+    fetch("http://localhost:3000/poems", {
+       method: "POST",
+       headers: {
+           "Content-Type": "application/json",
+           "Accept": "application/json",
+       },
+       body: JSON.stringify(poem)
+    })
+    .then(r => {
+        r.json();
+    })
+    .then (function(obj) {
+       console.log('inside postPoem');
+       console.log(obj);
+       deleteFirst();
+    })
+}
+
 document.addEventListener('DOMContentLoaded', function(e){
     let showForm = false;
     let poems = [];
-
-    function renderPoems(){
-        fetch("http://localhost:3000/poems/")
-        .then(r => r.json())
-        .then(data => {
-            console.log(data);
-            data.slice().reverse().forEach(poem => {
-                renderPoem(poem)
-            });
-            console.log('POEMS RENDERED');
-        });
-    }
 
     renderPoems();
 
@@ -32,56 +53,29 @@ document.addEventListener('DOMContentLoaded', function(e){
         const signed = e.target.signature.value;
         const image = e.target.imglink.value;
         const poemtxt = e.target.poem.value;
+
         postPoem(makePoem(signed, image, poemtxt));
-
-        deleteFirst();
-
-        document.querySelector(".poems").innerText = '';
-
         renderPoems();
-        
-        // fetch("http://localhost:3000/poems/")
-        // .then(r => r.json())
-        // .then(data => {
-        //     console.log(data);
-        // });
-
-    //     //posts text, pushes previous text
-    //     let poemCollection = document.querySelectorAll('.poem');
-    //     for (i = 12; i > 1; i--){
-    //         document.querySelector(`.poem:nth-child(${i})`).querySelector('.poemtxt').innerText = document.querySelector(`.poem:nth-child(${i - 1})`).querySelector('.poemtxt').innerText;
-    //     }
-    //     poemCollection[0].querySelector('.poemtxt').innerText = e.target.poem.value;
-
-    //     //posts image, pushes previous images
-    //     for (i = 12; i > 1; i--){
-    //         document.querySelector(`.poem:nth-child(${i})`).querySelector('img').src = document.querySelector(`.poem:nth-child(${i - 1})`).querySelector('img').src;
-    //     }
-    //     poemCollection[0].querySelector('img').src = e.target.imglink.value;
-
-    //     for (i = 12; i > 1; i--){
-    //         document.querySelector(`.poem:nth-child(${i})`).querySelector('.signed').innerText = document.querySelector(`.poem:nth-child(${i - 1})`).querySelector('.signed').innerText;
-    //     }
-    //     poemCollection[0].querySelector('.signed').innerText = e.target.signature.value;
 
         e.target.reset();
     })
-    function renderPoem(poem){
-        const d = document.createElement('div');
-        d.className = 'poem';
-        const s = document.createElement('p');
-        s.className = 'signed';
-        s.innerText = poem.signature;
-        const p = document.createElement('p');
-        p.className = 'poemtxt';
-        p.innerText = poem.poemtxt;
-        const i = document.createElement('img');
-        i.className = 'image';
-        i.src = poem.image;
-        d.append(s, i, p);
-        document.querySelector(".poems").append(d);
-    }
 })
+
+function renderPoem(poem){
+    const d = document.createElement('div');
+    d.className = 'poem';
+    const s = document.createElement('p');
+    s.className = 'signed';
+    s.innerText = poem.signature;
+    const p = document.createElement('p');
+    p.className = 'poemtxt';
+    p.innerText = poem.poemtxt;
+    const i = document.createElement('img');
+    i.className = 'image';
+    i.src = poem.image;
+    d.append(s, i, p);
+    document.querySelector(".poems").append(d);
+}
 
 function makePoem(signed, imagelink, poemtext) {
     const poem = {
@@ -90,34 +84,6 @@ function makePoem(signed, imagelink, poemtext) {
         poemtxt: poemtext
     }
     return poem;
-}
-
-function postPoem (poem) {
-     //add poem to db.json
-    fetch("http://localhost:3000/poems", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-        },
-        body: JSON.stringify(poem)
-    })
-    .then(r => r.json())
-    .then (function(obj) {
-        console.log('inside postPoem');
-        console.log(obj);
-    })
-
-    //delete first poem on db.json
-    // fetch("http://localhost:3000/poems/")
-    // .then(r => r.json())
-    // .then(data => {
-    //     fetch('http://localhost:3000/poems/' + data[0].id, {
-    //         method: 'DELETE'
-    //     })
-    //     .then( r => r.json())
-    //     .then(data => console.log(data));
-    // });
 }
 
 function deleteFirst(){
@@ -144,6 +110,7 @@ function deleteFirst(){
             .then(data => {
                 console.log('POEM DELETED');
                 console.log(data);
+                renderPoems();
             });
     }
 }
